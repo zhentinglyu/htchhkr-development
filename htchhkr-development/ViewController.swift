193f77e8bb71cfc7442e9a304adb382668899807
Zhenting Lu <zhentinglu@lumbpro2018.local>
2018-10-01 09:30:38 -0700

    Initial Commit

---
+++ htchhkr-development/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  htchhkr-development
+//
+//  Created by Zhenting Lu on 10/1/18.
+//  Copyright © 2018 Zhenting Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
